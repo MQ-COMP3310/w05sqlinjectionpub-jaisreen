e2e9df57a0308b61d4ab7676e6015b85f2420b60
jaisreen <jaisree.nathan1@students.mq.edu.au>
2025-03-28 10:40:28 +1100

    task 5.3
    5.3 completed

--- app/src/main/java/workshop05code/App.java
+++ app/src/main/java/workshop05code/App.java
@@ -1,26 +1,17 @@
 package workshop05code;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
-//Included for the logging exercise
-import java.io.FileInputStream;
-import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
-/**
- *
- * @author sqlitetutorial.net
- */
 public class App {
-    // Start code for logging exercise
     static {
-        // must set before the Logger
-        // loads logging.properties from the classpath
-        try {// resources\logging.properties
+        try {
             LogManager.getLogManager().readConfiguration(new FileInputStream("resources/logging.properties"));
         } catch (SecurityException | IOException e1) {
             e1.printStackTrace();
@@ -28,11 +19,7 @@ public class App {
     }
 
     private static final Logger logger = Logger.getLogger(App.class.getName());
-    // End code for logging exercise
-    
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         SQLiteConnectionManager wordleDatabaseConnection = new SQLiteConnectionManager("words.db");
 
@@ -43,6 +30,7 @@ public class App {
             System.out.println("Not able to connect. Sorry!");
             return;
         }
+
         if (wordleDatabaseConnection.createWordleTables()) {
             System.out.println("Wordle structures in place.");
         } else {
@@ -50,37 +38,42 @@ public class App {
             return;
         }
 
-        // let's add some words to valid 4 letter words from the data.txt file
-
+        // Add valid words from the file
         try (BufferedReader br = new BufferedReader(new FileReader("resources/data.txt"))) {
             String line;
             int i = 1;
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
-                wordleDatabaseConnection.addValidWord(i, line);
-                i++;
+                if (line.matches("^[a-z]{4}$")) {
+                    wordleDatabaseConnection.addValidWord(i, line);
+                    i++;
+                } else {
+                    System.out.println("Ignored invalid word from file: '" + line + "'");
+                }
             }
-
         } catch (IOException e) {
-            System.out.println("Not able to load . Sorry!");
+            System.out.println("Not able to load. Sorry!");
             System.out.println(e.getMessage());
             return;
         }
 
-        // let's get them to enter a word
-
+        // Accept guesses from the user
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Enter a 4 letter word for a guess or q to quit: ");
             String guess = scanner.nextLine();
 
             while (!guess.equals("q")) {
-                System.out.println("You've guessed '" + guess+"'.");
+				if(guess.matches("^[a-z]{4}$")){
+					System.out.println("You've guessed '" + guess+"'.");
 
-                if (wordleDatabaseConnection.isValidWord(guess)) { 
-                    System.out.println("Success! It is in the the list.\n");
-                }else{
-                    System.out.println("Sorry. This word is NOT in the the list.\n");
-                }
+					if (wordleDatabaseConnection.isValidWord(guess)) { 
+						System.out.println("Success! It is in the the list.\n");
+					}else{
+						System.out.println("Sorry. This word is NOT in the the list.\n");
+					}
+				}
+				else {
+					System.out.println("The word '" + guess + "' is not a valid word.");
+				}
 
                 System.out.print("Enter a 4 letter word for a guess or q to quit: " );
                 guess = scanner.nextLine();
@@ -88,6 +81,5 @@ public class App {
         } catch (NoSuchElementException | IllegalStateException e) {
             e.printStackTrace();
         }
-
     }
 }
